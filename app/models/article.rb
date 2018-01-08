f1333d4cf857505f643741d7bc7ed9f55f25f063
kristiee <smaskay@ymail.com>
2018-01-08 15:43:30 +0545

    deploy rails app

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
-class Article <ActiveRecord::Base
-validates: title, presence:true, length:{minimum:3, maximum:50}
-validates: description, presence:true, length:{minimum:10, maximum:300}
-end
+class Article < ActiveRecord::Base
+
+    validates :title, presence: true, length: {minimum: 3, maximum: 50}
+    
+    validates :desription, presence: true, length: {minimum: 10, maximum: 300}
+    
+    end
